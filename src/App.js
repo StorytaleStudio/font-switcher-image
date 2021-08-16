627ab70db8896358b352a012ed3ef1593b4565cc
Josh Tregenza <josh@joshtregenza.com>
2021-08-16 11:17:44 +1000

    update the font switcher

--- src/App.js
+++ src/App.js
@@ -5,21 +5,72 @@ import Logos from './components/logos/logos'
 import {ReactComponent as MakeLogo} from './logo.svg'
 import {ReactComponent as ReactLogo} from './react-logo.svg'
 
-function App({ headline, showLogos, backgroundImage }) {
+function App({ headline, subheading, showLogos, backgroundImage }) {
   return (
     <div className="App" style={{ backgroundImage: `url('${backgroundImage}')` }}>
       {showLogos && (
         <Logos logoOne={<MakeLogo />} logoTwo={<ReactLogo />} />
       )}
 
-      <h1>{headline}</h1>
+      <h1 class="hero">{headline}</h1>
+        <h2>{subheading}</h2> 
 
-      <div className="shape-container">
-        <Shapes />
-      </div>
     </div>
   );
 }
+
+const fonts = [
+  'Roboto', 
+  'Open Sans', 
+  'Raleway', 
+  'Georama', 
+  'Poppins', 
+  'Josefin Sans', 
+  'Fjalla One', 
+  'Comfortaa', 
+  'Righteous', 
+  'Public Sans', 
+  'Concert One', 
+  'Economica', 
+  'Droid Sans', 
+  'PT Mono', 
+  'Viga',
+  'Cabin Condensed',
+  'Pallette Mosaic',
+  'Coda',
+  'Fredericka the Great',
+  'Averia Serif Libre',
+  'Nova Round'
+];
+
+  WebFont.load({
+    google: {
+      families: fonts
+    }
+  });
+
+function fontSwitch(){
+  let h1 = document.querySelector(".hero"); // Get reference to the h1
+let text = h1.textContent.split("");                 // Get the text content into an array
+
+let result = ""; // Will hold output
+
+  
+// Loop over the array
+text.forEach(function(char){
+  
+   var rand = fonts[Math.floor(Math.random() * fonts.length)];
+  // Append a new span only if the current char is not a space
+  result += (char.trim() === "") ? "" : "<span style='font-family:" + rand + ";'>" + char + "</span>";
+});
+
+h1.innerHTML = result;  // Reset the h1's content
+
+// console.log(h1.outerHTML); // Test
+}
+
+
+setInterval(fontSwitch, 750);
 
 App.propTypes = {
   headline: PropTypes.string,
@@ -28,8 +79,9 @@ App.propTypes = {
 }
 
 App.defaultProps = {
-  headline: 'Hello World',
-  showLogos: true,
+  headline: 'storytale.',
+  subheadline: 'tell your story',
+  showLogos: false,
   backgroundImage: '',
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'typeface-ibm-plex-sans';
+import   'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js'
 
 ReactDOM.render(
   <React.StrictMode>
