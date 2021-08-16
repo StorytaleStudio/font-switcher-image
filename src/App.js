2954398b3812a9ce35bf144769f03d796078dacb
Josh Tregenza <josh@joshtregenza.com>
2021-08-16 11:25:03 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,60 @@ import {ReactComponent as MakeLogo} from './logo.svg'
 import {ReactComponent as ReactLogo} from './react-logo.svg'
 
 function App({ headline, subheading, showLogos, backgroundImage }) {
+  const fonts = [
+    'Roboto', 
+    'Open Sans', 
+    'Raleway', 
+    'Georama', 
+    'Poppins', 
+    'Josefin Sans', 
+    'Fjalla One', 
+    'Comfortaa', 
+    'Righteous', 
+    'Public Sans', 
+    'Concert One', 
+    'Economica', 
+    'Droid Sans', 
+    'PT Mono', 
+    'Viga',
+    'Cabin Condensed',
+    'Pallette Mosaic',
+    'Coda',
+    'Fredericka the Great',
+    'Averia Serif Libre',
+    'Nova Round'
+  ];
+  
+    WebFont.load({
+      google: {
+        families: fonts
+      }
+    });
+  
+  function fontSwitch(){
+    let h1 = document.querySelector(".hero"); // Get reference to the h1
+  let text = h1.textContent.split("");                 // Get the text content into an array
+  
+  let result = ""; // Will hold output
+  
+    
+  // Loop over the array
+  text.forEach(function(char){
+    
+     var rand = fonts[Math.floor(Math.random() * fonts.length)];
+    // Append a new span only if the current char is not a space
+    result += (char.trim() === "") ? "" : "<span style='font-family:" + rand + ";'>" + char + "</span>";
+  });
+  
+  h1.innerHTML = result;  // Reset the h1's content
+  
+  // console.log(h1.outerHTML); // Test
+  }
+  
+  
+  setInterval(fontSwitch, 750);
+
+
   return (
     <div className="App" style={{ backgroundImage: `url('${backgroundImage}')` }}>
       {showLogos && (
@@ -19,58 +73,7 @@ function App({ headline, subheading, showLogos, backgroundImage }) {
   );
 }
 
-const fonts = [
-  'Roboto', 
-  'Open Sans', 
-  'Raleway', 
-  'Georama', 
-  'Poppins', 
-  'Josefin Sans', 
-  'Fjalla One', 
-  'Comfortaa', 
-  'Righteous', 
-  'Public Sans', 
-  'Concert One', 
-  'Economica', 
-  'Droid Sans', 
-  'PT Mono', 
-  'Viga',
-  'Cabin Condensed',
-  'Pallette Mosaic',
-  'Coda',
-  'Fredericka the Great',
-  'Averia Serif Libre',
-  'Nova Round'
-];
 
-  WebFont.load({
-    google: {
-      families: fonts
-    }
-  });
-
-function fontSwitch(){
-  let h1 = document.querySelector(".hero"); // Get reference to the h1
-let text = h1.textContent.split("");                 // Get the text content into an array
-
-let result = ""; // Will hold output
-
-  
-// Loop over the array
-text.forEach(function(char){
-  
-   var rand = fonts[Math.floor(Math.random() * fonts.length)];
-  // Append a new span only if the current char is not a space
-  result += (char.trim() === "") ? "" : "<span style='font-family:" + rand + ";'>" + char + "</span>";
-});
-
-h1.innerHTML = result;  // Reset the h1's content
-
-// console.log(h1.outerHTML); // Test
-}
-
-
-setInterval(fontSwitch, 750);
 
 App.propTypes = {
   headline: PropTypes.string,
