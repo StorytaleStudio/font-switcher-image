cb0d91aa4b4e4be729406ae7fedc496717d6602c
Josh Tregenza <josh@joshtregenza.com>
2021-08-17 09:52:38 +1000

    Another Build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import PropTypes from "prop-types";
 import './App.css';
-import Shapes from './components/shapes/shapes'
 import Logos from './components/logos/logos'
 import {ReactComponent as MakeLogo} from './logo.svg'
 import {ReactComponent as ReactLogo} from './react-logo.svg'
